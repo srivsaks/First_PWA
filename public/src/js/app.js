ec1b6ea3289a6f446746808ba4f56cf1b3efcd8d
Sakshi Srivastava <srivsaks@amazon.com>
2022-04-03 19:55:01 +0530

    static caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,13 @@
-
+var deferredPrompt
 // checks if browser supports service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("/sw.js").then(function () {
     console.log("Service worker registered");
     })
 }
+
+window.addEventListener("beforeinstallprompt",function(e){
+e.preventDefault();
+deferredPrompt=e;
+return false;
+})
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,29 @@
+self.addEventListener("install", function (event) {
+    //console.log("[Service Worker Installed] ...", event);
+    // caches.open is an async operation, if we dont use waitUntil then it might happen that installation ends and cache hasnt got created yet and service worker's fetch is trying to access the cache and it might cause some issue.
+    event.waitUntil(caches.open("precache").then(function (cache) { // precache is the name given to the cache
+        console.log(cache);
+        cache.addAll(["/","/index.html","/src/js/app.js","/src/js/feed.js","/src/js/material.min.js","/src/css/app.css","/src/css/feed.css","/src/images/main-image.jpg"])
+    }))
+})
 
-// checks if browser supports service worker
-if('serviceWorker' in navigator){
-    
-}
+self.addEventListener("activate", function (event) {
+    //console.log("[Service Worker Activated] ...", event);
+    return self.clients.claim();
+})
+
+self.addEventListener("fetch", function (event) {
+    //console.log("[Service Worker Fetching] ...", event);
+    // below will not render any content on the screen and you will get the site cant be reached
+    // event.respondWith(null)
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+            }
+            else {
+                return fetch(event.request);
+            }
+        })
+    );
+})
